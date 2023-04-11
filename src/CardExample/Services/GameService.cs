1d9f85a95e49b3a79775476579dc379ec244e8d9
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-04-10 22:30:11 -0500

    Created a basic game service.

---
+++ src/CardExample/Services/GameService.cs
@@ -0,0 +1,44 @@
+namespace CardExample.Services;
+
+/// <summary>
+/// Represents a service for managing the game.
+/// </summary>
+public sealed class GameService {
+    private readonly PlayerService _playerService;
+    private readonly DeckService _deckService;
+
+    public GameService(PlayerService playerService, DeckService deckService) {
+        _playerService = playerService;
+        _deckService = deckService;
+    }
+
+    /// <summary>
+    /// Runs the game to completion.
+    /// </summary>
+    public void Run() {
+        do {
+            Player nextPlayer = _playerService.GetNextPlayer();
+
+            // Get a target at random for demonstration purposes.
+            // This can be any player, including the sender.
+            // Normally the player would select a target directly.
+            Player target = _playerService.GetRandomTarget();
+
+            // Pick one of the player's cards at random.
+            // This is only random for the example.
+            Card card = nextPlayer.Hand[random.Next(0, nextPlayer.Hand.Count)];
+
+            // Be sure to remove the card from the player's hand as we play it.
+            nextPlayer.Hand.Remove(card);
+
+            // Apply any behaviors associated with the card.
+            foreach (CardBehavior behavior in card.Behaviors)
+                behavior.Apply(target);
+
+            // Apply the immediate impact of the card.
+            target.Health -= card.Value;
+
+            // Continue iterating until only one player stands.
+        } while (players.Where(p => p.Health > 0).Count() > 1);
+    }
+}
