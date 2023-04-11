ba6109fee75e9874fbf49456d136dac9205a417f
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-04-10 21:21:04 -0500

    Created a card model.

---
+++ src/CardExample/Card.cs
@@ -0,0 +1,15 @@
+namespace CardExample;
+
+/// <summary>
+/// Represents a basic card for gameplay.
+/// </summary>
+public class Card {
+    /// <summary>
+    /// The name of the card.
+    /// </summary>
+    public string Name { get; set; }
+    /// <summary>
+    /// The value of the card.
+    /// </summary>
+    public double Value { get; set; }
+}
