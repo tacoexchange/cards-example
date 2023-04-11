707957448bc986202896b6306295fc13411e0c53
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-04-10 22:21:33 -0500

    Created a player service.

---
+++ src/CardExample/Services/PlayerService.cs
@@ -0,0 +1,34 @@
+namespace CardExample.Services;
+
+/// <summary>
+/// Represents a service for managing players.
+/// </summary>
+public sealed class PlayerService
+{
+    private readonly List<Player> _players;
+
+    /// <summary>
+    /// Creates a new <see cref="PlayerService"/> instance.
+    /// </summary>
+    public PlayerService() =>
+        _players = new List<Player>();
+
+    /// <summary>
+    /// Adds the specified player to the game.
+    /// </summary>
+    /// <param name="player">The player to add.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="player"/> is <see langword="null"/>.</exception>
+    public void AddPlayer(IEnumerable<Card> initialHand) {
+        if (initialHand is null)
+            throw new ArgumentNullException(nameof(initialHand));
+
+        var player = new Player {
+            Id = _players.Count + 1,
+            Health = 100,
+            Mana = 100,
+            Hand = new List<Card>(initialHand)
+        };
+
+        _players.Add(player);
+    }
+}
