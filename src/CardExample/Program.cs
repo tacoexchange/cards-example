4128b3b1a7f2989497a81d335df5125705dd07b7
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-04-10 21:19:37 -0500

    Created entry point for console.

---
+++ src/CardExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardExample;
+
+internal class Program
+{
+    internal static void Main()
+    {
+        Console.WriteLine("Press any key to exit.");
+        _ = Console.ReadKey();
+    }
+}
