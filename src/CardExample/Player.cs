c5d77da8ab47b02284529f7c8f9a5c851b1f5496
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-04-10 21:44:45 -0500

    Created a basic player model.

---
+++ src/CardExample/Player.cs
@@ -0,0 +1,23 @@
+namespace CardExample;
+
+/// <summary>
+/// Represents a player.
+/// </summary>
+public sealed class Player {
+    /// <summary>
+    /// Gets or sets the player's unique identifier.
+    /// </summary>
+    public int Id { get; set; }
+    /// <summary>
+    /// Gets or sets the player's health.
+    /// </summary>
+    public double Health { get; set; }
+    /// <summary>
+    /// Gets or sets the player's mana.
+    /// </summary>
+    public double Mana { get; set; }
+    /// <summary>
+    /// Gets or sets the player's hand.
+    /// </summary>
+    public List<Card> Hand { get; set; }
+}
